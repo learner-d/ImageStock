af85930fd8d34dff5c0cc7cc758dc7c8d83ab2ac
learner-d <34832600+learner-d@users.noreply.github.com>
2020-12-03 17:07:15 +0200

    Add project files.

---
+++ ImageStock/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageStock.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageStock.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly IGallery gallery;
+        public GalleryController(IGallery _gallery)
+        {
+            gallery = _gallery;
+        }
+        public IActionResult Index()
+        {
+            return View(gallery);
+        }
+    }
+}
---
+++ ImageStock/Data/Models/Gallery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageStock.Data.Models
+{
+    public class Gallery
+    {
+        protected List<Post> posts;
+        public IReadOnlyList<Post> Posts => posts;
+    }
+}
